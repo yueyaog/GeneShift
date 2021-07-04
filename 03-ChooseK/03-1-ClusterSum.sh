7c6a3c165330a23d05e7b1110686b05f13b6cc14
yueyaog <yueyaog@clemson.edu>
2021-07-03 23:45:49 -0400

    Fixed a variable name problem in 03-1 script

--- 03-ChooseK/03-1-ClusterSum.sh
+++ 03-ChooseK/03-1-ClusterSum.sh
@@ -4,7 +4,7 @@ BASEDIR=$(pwd)
 
 mkdir ${BASEDIR}/ClusterSumPBS
 
-cp ${BASEDIR}/Compile_clusters.py ${BASEDIR}/ClusterSumPBS/
+cp ${BASEDIR}/Cluster_Compile.py ${BASEDIR}/ClusterSumPBS/
 
 for i in {35..90..5} ; do cat ${BASEDIR}/Cluster_Summary.template | sed s/KX/${i}/g > ${BASEDIR}/ClusterSumPBS/K${i}.Cluster_Sum.pbs ; done
 
